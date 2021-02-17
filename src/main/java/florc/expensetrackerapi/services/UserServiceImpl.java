d8591382939e10e7d1baa2f7529ca66b91bc42b4
Flor <florencia.corrado94@gmail.com>
2021-02-17 12:14:30 -0300

    Add implementations

--- src/main/java/florc/expensetrackerapi/services/UserServiceImpl.java
+++ src/main/java/florc/expensetrackerapi/services/UserServiceImpl.java
@@ -2,8 +2,22 @@ package florc.expensetrackerapi.services;
 
 import florc.expensetrackerapi.domain.User;
 import florc.expensetrackerapi.exceptions.EtAuthException;
+import florc.expensetrackerapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+
+@Service
+@Transactional
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public User validateUser(String email, String password) throws EtAuthException {
         return null;
@@ -11,6 +25,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User registerUser(String firstName, String lastname, String email, String password) throws EtAuthException {
-        return null;
+        Pattern pattern = Pattern.compile("^(.+)@(.+)$");
+            if (email != null) email = email.toLowerCase();
+            if (!pattern.matcher(email).matches())
+                throw new EtAuthException("Invalidad email format");
+            Integer count = userRepository.getCountByEmail(email);
+            if (count > 0)
+                throw new EtAuthException("Email already in use");
+        Integer userId = userRepository.create(firstName, lastname, email, password);
+        return userRepository.findById(userId);
     }
 }
